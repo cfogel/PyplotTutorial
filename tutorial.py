c144b7b20615b9c36dd97f42d738fa19005ef92c
CJ Fogel <fogelcj@gmail.com>
2014-01-26 18:37:07 -0500

    second example

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,15 @@
+import numpy as np
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
+
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000000)
+
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100, \ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
 plt.show()
